11eb594d3b5b96fae35471b8c8bc99fb8e6f4ea4
Ivan Sakhno <sakhno.iv@gmail.com>
2019-11-11 00:31:58 +0300

    Initialised Spring Boot project.

---
+++ src/main/java/com/scarycrawler/filmtracker/FilmtrackerApplication.java
@@ -0,0 +1,13 @@
+package com.scarycrawler.filmtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmtrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scarycrawler/filmtracker/FilmtrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scarycrawler.filmtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmtrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
